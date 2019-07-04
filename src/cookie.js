cd903536a532e8d5f62b40d31111f6b798d6c060
Austin Jackson <themacphage@gmail.com>
2019-07-03 22:48:23 -0500

    proxy boilerplate

--- src/cookie.js
+++ src/cookie.js
@@ -2,6 +2,7 @@
 const tough = require('tough-cookie');
 
 module.exports = {
+    // Converts puppeteer cookie value to a tough.Cookie
     puppeteerToTough: cookie => {
         return new tough.Cookie({
             key: cookie.name,
@@ -14,38 +15,3 @@ module.exports = {
         })
     }
 };
-
-/*
-
-PUPPETEER
-
-{ name: '__RequestVerificationToken',
-    value:
-     'YfTaaZogwqXtvwbRiTGIBDHeGaS8l4zxQWFt_ptiJZ3e5h1Duss1ZsGDHfmollj0e7yMKnvoV1Bw-pdUqBprEWP09a81',
-    domain: 'uh.collegescheduler.com',
-    path: '/',
-    expires: -1,
-    size: 118,
-    httpOnly: true,
-    secure: true,
-    session: true }
-
-
-TOUGH
-
-Cookie object properties:
-
-    key - string - the name or key of the cookie (default "")
-    value - string - the value of the cookie (default "")
-    expires - Date - if set, the Expires= attribute of the cookie (defaults to the string "Infinity"). See setExpires()
-    maxAge - seconds - if set, the Max-Age= attribute in seconds of the cookie. May also be set to strings "Infinity" and "-Infinity" for non-expiry and immediate-expiry, respectively. See setMaxAge()
-    domain - string - the Domain= attribute of the cookie
-    path - string - the Path= of the cookie
-    secure - boolean - the Secure cookie flag
-    httpOnly - boolean - the HttpOnly cookie flag
-    extensions - Array - any unrecognized cookie attributes as strings (even if equal-signs inside)
-    creation - Date - when this cookie was constructed
-    creationIndex - number - set at construction, used to provide greater sort precision (please see cookieCompare(a,b) for a full explanation)
-
-
-*/
